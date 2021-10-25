bfa8f9cb6e568ebeb64eeef94840412337ddaf01
MitoMonkey <85686133+MitoMonkey@users.noreply.github.com>
2021-10-25 11:19:28 +0200

    registration form implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
+
 import { FormGroup, FormControl } from '@angular/forms';
+
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
@@ -9,19 +13,29 @@ import { FormGroup, FormControl } from '@angular/forms';
 export class AppComponent {
   title = 'MoveX-Angular-client';
 
-  // For the template-driven form
-  value = ''
-  getValue() {
-    alert(this.value);
+  constructor(public dialog: MatDialog) { }
+
+  // open a dialog when the signup button is clicked (the dialog is defined in user-registration-form.component.ts)
+  openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, {
+      width: '280px'
+    });
   }
 
-  // for the reactive form
-  valueForm = new FormGroup({ // "valueForm" is the name of the form
-    input: new FormControl('type something here'), // in the () is a placeholder
-    username: new FormControl()
-  });
-  onSubmit() {
-    console.log(this.valueForm.value);
-  }
+
+  /*   // For the dummy template-driven form
+    value = ''
+    getValue() {
+      alert(this.value);
+    }
+  
+    // for dummy the reactive form
+    valueForm = new FormGroup({ // "valueForm" is the name of the form
+      input: new FormControl('type something here'), // in the () is a placeholder
+      username: new FormControl()
+    });
+    onSubmit() {
+      console.log(this.valueForm.value);
+    } */
 
 }
--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
-@Component({
+// to close the dialog on success
+import { MatDialogRef } from '@angular/material/dialog';
+// display notifications back to the user
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+// API calls service
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+@Component({ // tell Angular that the class right below is a component
   selector: 'app-user-registration-form',
   templateUrl: './user-registration-form.component.html',
   styleUrls: ['./user-registration-form.component.scss']
 })
+
 export class UserRegistrationFormComponent implements OnInit {
 
-  constructor() { }
+  @Input() userData = { Username: '', Password: '', Email: '', Birthday: '' };
 
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<UserRegistrationFormComponent>,
+    public snackBar: MatSnackBar
+  ) { }
+
+  // ngOnInit method is called once the component has received all its inputs (all its data-bound properties)
   ngOnInit(): void {
+  }
+
+  // function to send form inputs to the backend
+  registerUser(): void {
+    this.fetchApiData.userRegistration(this.userData).subscribe((result) => {
+
+      // Logic for a successful user registration goes here! (To be implemented)
+
+      this.dialogRef.close(); // This will close the modal on success. (it is opened in the root component method openUserRegistrationDialog)
+      console.log(result);
+
+      this.snackBar.open(result, 'OK', {
+        duration: 2000
+      });
+    }, (result) => {
+      console.log(result);
+      this.snackBar.open(result, 'OK', {
+        duration: 2000
+      });
+    });
   }
 
 }
